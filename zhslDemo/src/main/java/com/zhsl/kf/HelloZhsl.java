a58cf71ce6642901c6e4311a0c02d96e26fe58c8
hjlabc321 <604067483@qq.com>
2021-12-24 00:48:29 +0800

    test

--- zhslDemo/src/main/java/com/zhsl/kf/HelloZhsl.java
+++ zhslDemo/src/main/java/com/zhsl/kf/HelloZhsl.java
@@ -4,10 +4,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class HelloZhsl {
-    public void hello(){
+@RequestMapping("/hi")
+    public String hello(){
         System.out.println("hello");
+        return "hello";
     }
 
 }
